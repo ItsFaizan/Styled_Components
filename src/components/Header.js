f690d42f00aae5a729b97c5a5aa09c38e35820c4
ItsFaizan <faizanjavid71@gmail.com>
2023-06-21 23:52:05 +0500

    Styled Components Imported and props passed

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyledHeader } from './Header.styled.js';
+
+ const FirstComponent = () => {
+               
+         return (
+
+           <StyledHeader bg = 'red'>   
+            <h1>Styled Components</h1>
+           </StyledHeader>
+
+    )
+ }
+
+ export default FirstComponent;
