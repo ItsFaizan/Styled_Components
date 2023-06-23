b346c72fa6c3b59e5be513660571df082a566aec
ItsFaizan <faizanjavid71@gmail.com>
2023-06-23 23:14:20 +0500

    Landing Page fully completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Header from './components/Header.js'
 import { ThemeProvider } from 'styled-components';
 import  GlobalStyles  from './components/styles/Global.js';
-
+import { Container } from './components/styles/Container.styled.js';
+import Card from './components/Card.js';
+import content from './content.js';
+import Footer from './components/Footer.js';
 const theme = {
     colors: {
       header: '#ebfbff',
@@ -17,6 +20,12 @@ const App = () => {
   <>
   <GlobalStyles />
   <Header />
+  <Container>
+    {content.map((item, index)=>(
+        <Card key={index} item={item} />
+    ))}
+  </Container>
+  <Footer/>
   </>
   </ThemeProvider>
  );
